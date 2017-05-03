3a17ff4604b9d47ca6f47b94f1af251aa66ca68f
Quinn Perfetto <thequinnftw@gmail.com>
2017-05-03 14:53:45 -0700

    make up an index

--- models/host.py
+++ models/host.py
@@ -4,4 +4,4 @@ class Host(RedisModel):
     ip_address   = Column(type=str, primary_key=True)
     scanned_at   = Column(type=str, required=True)
     scan_results = Column(type=str, required=True)
-    up           = Column(type=int, required=True)
+    up           = Column(type=int, required=True, index=True)
