82c33e15f88cc45d8b967c28cf797cbe263d0abb
Quinn Perfetto <thequinnftw@gmail.com>
2017-05-03 14:52:57 -0700

    optimized ping sweeper

--- cron/ping_sweep.py
+++ cron/ping_sweep.py
@@ -1,3 +1,8 @@
+# Ping sweep the local network and store the results in Redis.
+#
+# The python-nmap package does include an AsyncPortScanner module but it is
+# very slow, so I have rewritten this functionality using python3 asyncio.
+
 # TODO: There has to be a better way to do relative imports than this garbage...
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -5,40 +10,49 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import asyncio
 import nmap
 import ipaddress
+import json
+from concurrent.futures import ThreadPoolExecutor
 
 import settings
 from models.host import Host
 import db
 
-scanner = nmap.PortScanner()
+executor = ThreadPoolExecutor()
+loop     = asyncio.get_event_loop()
 
+# Check if the results point a host that is currently online.
 def is_up(scan_result):
     return int(scan_result["nmap"]["scanstats"]["uphosts"]) > 0
 
+# Scan the host and store the results in Redis.
 async def scan_and_report(host, redis_connection):
     print("scanning", host)
-    result = scanner.scan(host, arguments="-sn")
+    scanner  = nmap.PortScanner()
+    # Run in executor doesn't allow for kwargs, so the arguments passed here
+    # are: self, host, ports, arguments.
+    result = await loop.run_in_executor(executor, nmap.PortScanner.scan,
+            scanner, host, None, "-sn")
     entry = Host(
         ip_address   = host,
         scanned_at   = result["nmap"]["scanstats"]["timestr"],
-        scan_results = str(result),
+        scan_results = json.dumps(result),
         up           = int(is_up(result))
 
     )
     await entry.save(redis_connection)
 
+# Generate the IP addresses of all hosts within the provided network mask.
 def host_generator():
     yield from map(str, ipaddress.ip_network(settings.NETWORK_MASK))
 
+# Main worker function.
 async def scan_all():
     redis_connection = await db.connect()
-    print(redis_connection)
     scan_tasks = [scan_and_report(host, redis_connection)
                   for host in host_generator()]
     await asyncio.wait(scan_tasks)
     redis_connection.close()
     await redis_connection.wait_closed()
 
-loop  = asyncio.get_event_loop()
 loop.run_until_complete(scan_all())
 loop.close()
